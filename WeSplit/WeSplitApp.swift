212cbdb5d886eaf742bb966cb16628b5013f4cb0
Emily Liang <eliang@lcs.on.ca>
2020-11-02 11:05:23 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Emily Liang on 2020-11-02.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
