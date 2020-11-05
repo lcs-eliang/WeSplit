0f1892061b062d928e0d27f532f3506fba7ed4bd
Emily Liang <eliang@lcs.on.ca>
2020-11-05 09:39:32 -0500

    Fixed the navigation title by putting it under the first thing after the navigation view; changed the number of people into a string and change the default value to 2 in case the user types a wrong answer. .

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -7,28 +7,26 @@
 
 import SwiftUI
 
-
-
 struct ContentView: View {
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = "2"
     @State private var tipPercentage = 2
     
     let tipPercentages = [10,15,20,25,0]
     
     var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+        let peopleCount = Double(numberOfPeople) ?? 2  // nil coalescing operator; when numberOfPeople can't be made into a double (like if someone typed a word, such as "hamburger") use a default value of 0
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0
         
         let tipValue = orderAmount / 100 * tipSelection
         let grandTotal = orderAmount + tipValue
         let amountPerPerson = grandTotal / peopleCount
-            
+        
         return amountPerPerson
     }
     
-    var body: some View{
+    var body: some View {
         NavigationView {
             Form {
                 Section {
@@ -36,35 +34,36 @@ struct ContentView: View {
                         .keyboardType(.decimalPad)
                     
                     
-                    Picker("Number of people", selection:
-                       $numberOfPeople) {
-                        ForEach(2 ..< 100) {
-                            Text("\($0) people")
-       
-                        }
-                       }
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.decimalPad)
+                    
                 }
                 
                 Section(header: Text("How much tip do you want to leave?")) {
                     Picker("Tip percentage", selection:
-                        $tipPercentage) {
+                            $tipPercentage) {
                         ForEach(0 ..< tipPercentages.count) {
                             Text("\(self.tipPercentages[$0])%")
                         }
                     }
                     .pickerStyle(SegmentedPickerStyle())
                 }
-                Section {
+                Section(header: Text("Amount per person:")){
                     Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
+                
             }
             .navigationBarTitle("WeSplit")
         }
+        
+        
+    }
+
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-        ContentView()
-        }
-    }
 
